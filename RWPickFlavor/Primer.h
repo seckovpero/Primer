8a8da6021028f50aedb6beafa3108663c77dbb9c
Pero <seckovpero@gmail.com>
2018-03-19 11:43:22 +0100

    changes

---
+++ RWPickFlavor/Primer.h
@@ -0,0 +1,13 @@
+//
+//  Primer.h
+//  RWPickFlavor
+//
+//  Created by AspidaMacBook on 19/03/2018.
+//  Copyright © 2018 AspidaMacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Primer : UIView
+
+@end
---
+++ RWPickFlavor/Primer.m
@@ -0,0 +1,21 @@
+//
+//  Primer.m
+//  RWPickFlavor
+//
+//  Created by AspidaMacBook on 19/03/2018.
+//  Copyright © 2018 AspidaMacBook. All rights reserved.
+//
+
+#import "Primer.h"
+
+@implementation Primer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
